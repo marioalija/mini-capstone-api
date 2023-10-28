54aa8c7379883eb68ba83eb8b31172df2d13f4b0
marioalija <marioalija25@gmail.com>
2023-10-28 13:27:44 -0500

    puts seeds in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,18 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# product = Product.new(name: "Apple Watch", price: 800, image_url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ML773_VW_34FR+watch-case-45-stainless-graphite-s9_VW_34FR+watch-face-45-stainless-graphite-s9_VW_34FR?wid=2000&hei=2000&fmt=png-alpha&.v=1694507905569", description: "Can
+#  do almost everything yoour phone can, but on your wrist")
+# product.save
+
+# product = Product.new(name: "IPhone 14Pro", price: 1200, image_url: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-14-pro-max/carousel/deeppurple/deeppurple-1.png", description: "The power of a computer in your hands")
+# product.save
+
+# product = Product.new(name: "PS5", price: 500, image_url: "https://i5.walmartimages.com/seo/Sony-PlayStation-5-Video-Game-Console_9333b8cd-773e-49d4-8e85-51c4114fcd56.bbf5c4e4f3746ddc28ac982117cdbf9a.jpeg", description: "The best video game experience on the market")
+# product.save
+
+# product = Product.new(name: "Oculus Quest", price: 1000, image_url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6473/6473553ld.jpg", description: "Enter different worlds of fantasy and freedom")
+# product.save
+
+# product = Product.new(name: "Water Bottle", price: 20, image_url: "https://www.hydroflask.com/media/catalog/product/w/3/w32bts600-berry-straighton_1.jpg", description: "Your beverage is always just the right temperature")
+# product.save
