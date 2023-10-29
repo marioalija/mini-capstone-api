9634c05894a06fa384852511d33ef2f2d91b9efe
marioalija <marioalija25@gmail.com>
2023-10-28 20:13:56 -0500

    added the create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    @product = Product.create(
+      name: "Sony TV",
+      price: 2000,
+      image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr0zuY28I0HoftTvVDFZ82xcjOaDCUYm8v_A&usqp=CAU",
+      description: "Best resolution and colors from any other television",
+    )
+    render :show
+  end
 end
