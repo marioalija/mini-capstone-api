9211ab34f1f6229752cd667db067c83f5d0ad30b
marioalija <marioalija25@gmail.com>
2023-10-31 14:32:54 -0500

    added images db

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,7 +1,7 @@
 json.id product.id
 json.name product.name
 json.price product.price
-json.image_url product.image_url
+json.images product.images
 json.description product.description
 json.description_list product.description_list
 json.is_discounted? product.is_discounted?
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,8 +25,8 @@
 # supplier = Supplier.new(name: "Facebook", email: "facebook@example.com", phone_number: "555-55-5555")
 # supplier.save
 
-# image = Image.create([{ url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ML773_VW_34FR+watch-case-45-stainless-graphite-s9_VW_34FR+watch-face-45-stainless-graphite-s9_VW_34FR?wid=2000&hei=2000&fmt=png-alpha&.v=1694507905569" }, { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/s9-case-unselect-gallery-1-202309_FMT_WHH?wid=752&hei=720&fmt=p-jpg&qlt=80&.v=1693524702398" }, { url: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MTG73ref_VW_34FR+watch-case-45-aluminum-midnight-cell-s9_VW_34FR+watch-face-45-aluminum-midnight-s9_VW_34FR_WF_CO_GEO_TH_LANG_TH?wid=2000&hei=2000&fmt=png-alpha&.v=1694507905569" }])
-# image = Image.create(url: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-14-pro-max/carousel/deeppurple/deeppurple-1.png")
-# image = Image.create(url: "https://i5.walmartimages.com/seo/Sony-PlayStation-5-Video-Game-Console_9333b8cd-773e-49d4-8e85-51c4114fcd56.bbf5c4e4f3746ddc28ac982117cdbf9a.jpeg")
-# image = Image.create(url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6473/6473553ld.jpg")
-# image = Image.create(url: "https://www.hydroflask.com/media/catalog/product/w/3/w32bts600-berry-straighton_1.jpg")
+image = Image.create([{ url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ML773_VW_34FR+watch-case-45-stainless-graphite-s9_VW_34FR+watch-face-45-stainless-graphite-s9_VW_34FR?wid=2000&hei=2000&fmt=png-alpha&.v=1694507905569" }, { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/s9-case-unselect-gallery-1-202309_FMT_WHH?wid=752&hei=720&fmt=p-jpg&qlt=80&.v=1693524702398" }, { url: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MTG73ref_VW_34FR+watch-case-45-aluminum-midnight-cell-s9_VW_34FR+watch-face-45-aluminum-midnight-s9_VW_34FR_WF_CO_GEO_TH_LANG_TH?wid=2000&hei=2000&fmt=png-alpha&.v=1694507905569" }])
+image = Image.create(url: "https://www.att.com/scmsassets/global/devices/phones/apple/apple-iphone-14-pro-max/carousel/deeppurple/deeppurple-1.png")
+image = Image.create(url: "https://i5.walmartimages.com/seo/Sony-PlayStation-5-Video-Game-Console_9333b8cd-773e-49d4-8e85-51c4114fcd56.bbf5c4e4f3746ddc28ac982117cdbf9a.jpeg")
+image = Image.create(url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6473/6473553ld.jpg")
+image = Image.create(url: "https://www.hydroflask.com/media/catalog/product/w/3/w32bts600-berry-straighton_1.jpg")
