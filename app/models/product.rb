395e6ffa19f5ef1cba772b82bf0e6570bcc4372f
marioalija <marioalija25@gmail.com>
2023-10-31 15:01:33 -0500

    changed json to show all images for a product

--- app/models/product.rb
+++ app/models/product.rb
@@ -32,7 +32,13 @@ class Product < ApplicationRecord
     created_at.strftime("%A, %d %b %Y %l:%M %p")
   end
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
 end
