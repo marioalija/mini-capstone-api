311ba5da13d0ed7b52e8aceea4d2fb436520ebee
marioalija <marioalija25@gmail.com>
2023-10-31 15:52:22 -0500

    change some of the image_urls in the test controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,12 +14,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "image_url", "description", "description_list", "is_discounted?", "tax", "total", "created_at", "friendly_created_at", "updated_at"], data.keys
+    assert_equal ["id", "name", "price", "images", "description", "description_list", "is_discounted?", "tax", "total", "created_at", "friendly_created_at", "updated_at", "supplier_id", "supplier"], data.keys
   end
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description" }
+      post "/products.json", params: { name: "test product", price: 1, images: "image.jpg", description: "test description" }
     end
   end
 
