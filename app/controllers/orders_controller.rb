fa7c7e6608a486e5ba11c1a060c1c1ad4b7adc8f
marioalija <marioalija25@gmail.com>
2023-11-15 15:01:38 -0600

    added tests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.where(user_id: current_user.id)
     render :index
   end
 
@@ -13,6 +13,7 @@ class OrdersController < ApplicationController
       tax: params[:tax],
       total: params[:total],
     )
+    @order.update(subtotal: @order.subtotal, tax: @order.tax, total: @order.total)
     if @order.save
       render :show
     else
@@ -22,6 +23,10 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by(id: params[:id])
-    render :show
+    if current_user && current_user.id == @order.user_id
+      render :show
+    else
+      render json: { message: "You can only view your own orders" }
+    end
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,19 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  def subtotal
+    @product = Product.find_by(id: product_id)
+    subtotal = @product[:price] * quantity
+    return subtotal
+  end
+
+  def tax
+    subtotal * 0.09
+  end
+
+  def total
+    total = subtotal + tax
+    return total
+  end
 end
