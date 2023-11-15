026d2a2fcadc28e3f927f75b68db1f31ef625a75
marioalija <marioalija25@gmail.com>
2023-11-15 15:53:10 -0600

    -m

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,35 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.where(user_id: current_user.id)
+    @orders = current_user.orders
     render :index
   end
 
   def create
+    product = Product.find_by(id: params[:product_id])
+
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.create(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
-    @order.update(subtotal: @order.subtotal, tax: @order.tax, total: @order.total)
-    if @order.save
+    # @order.update(subtotal: @order.subtotal, tax: @order.tax, total: @order.total)
+
+    if @order.save #happy path
       render :show
-    else
+    else #sad path
       render json: { errors: @order.errors.full_messages }, status: 400
     end
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
-    if current_user && current_user.id == @order.user_id
+    if (@order = current_user.order.find_by(id: params[:id]))
       render :show
     else
       render json: { message: "You can only view your own orders" }
--- app/views/orders/_order.json.jbuilder
+++ app/views/orders/_order.json.jbuilder
@@ -1,6 +1,7 @@
+json.id order.id
 json.user order.user
-json.product order.product
-json.quantity order.quantity
 json.subtotal order.subtotal
+json.quantity order.quantity
 json.tax order.tax
 json.total order.total
+json.product order.product
